dd96816209711947ef4a0c64e8935e22b220840c
yb <840823561@qq.com>
2019-10-24 16:41:26 +0800

    使用swiper构建testDetail页面进行中

--- src/components/test/TestDetail.vue
+++ src/components/test/TestDetail.vue
@@ -1,66 +1,99 @@
 <template>
-    <div>
-      <span>(单选)</span>
-      <span class="title">{{question.title}}</span>
-
-      <div class="options">
-        <div v-for="(value,key,index) in question.options" :key="index" @click="selectOption(index,key,question.isSingle)" :class="{active:answer.indexOf(key)!=-1}">
-          <span v-if="key=='A'" class="iconfont icon-icon-class-a"></span>
-          <span v-if="key=='B'" class="iconfont icon-icon-class-b"></span>
-          <span v-if="key=='C'" class="iconfont icon-icon-class-c"></span>
-          {{value}}
+  <mt-swipe :auto="0" style="height: 400px;background-color: #fff;"
+            :continuous=false
+            :showIndicators=false>
+    <mt-swipe-item v-for="question in questions" :key="question.id">
+      <div>
+        <span>(单选)</span>
+        <span class="title">{{question.title}}</span>
+        <div class="options">
+          <div v-for="(value,key,index) in question.options" :key="index"
+               @click="selectOption(index,key,question.isSingle)" :class="{active:answer.indexOf(key)!=-1}">
+            <span v-if="key=='A'" class="iconfont icon-icon-class-a"></span>
+            <span v-if="key=='B'" class="iconfont icon-icon-class-b"></span>
+            <span v-if="key=='C'" class="iconfont icon-icon-class-c"></span>
+            {{value}}
+            <div>aaaaaaaaaaaaaaaaa</div>
+          </div>
         </div>
       </div>
-    </div>
+    </mt-swipe-item>
+  </mt-swipe>
+
 </template>
 
 <script>
-    export default {
-        name: "TestDetail",
-      data(){
-          return {
-            question:{
-              id:1,
-              title:"this is question1,请问下列哈哈哈哈哈哈哈哈哈哈哈哈哈",
-              isSingle:true,
-              options:{
-                A:"this is A",
-                B:"this is B",
-                C:"this is C",
-                D:"this is D",
-              }
-            },
-            answer:[],
-          }
-      },
-      methods:{
-        selectOption(index,answer,isSingle){
-          console.log(answer);
-          if(isSingle){
-            //替换答案
-            this.$set(this.answer,0,answer)
-          }else{
-            //判断是否已选，已选则删除，未选则添加
-            if(this.answer.indexOf(answer)!=-1){
-              console.log(this.answer.indexOf(answer));
-              var index = this.answer.indexOf(answer)
-              this.answer.splice(index,1)
-            }else{
-              this.answer.push(answer)
+  export default {
+    name: "TestDetail",
+    data() {
+      return {
+        questions: [
+          {
+            id: 1,
+            title: "this is question1,请问下列哈哈哈哈哈哈哈哈哈哈哈哈哈",
+            isSingle: true,
+            options: {
+              A: "this is A",
+              B: "this is B",
+              C: "this is C",
+              D: "this is D",
+            }
+          },
+          {
+            id: 2,
+            title: "this is question2,请问下列哈哈哈哈哈哈哈哈哈哈哈哈哈",
+            isSingle: true,
+            options: {
+              A: "this is A",
+              B: "this is B",
+              C: "this is C",
+              D: "this is D",
+            }
+          },
+          {
+            id: 3,
+            title: "this is question3,请问下列哈哈哈哈哈哈哈哈哈哈哈哈哈",
+            isSingle: true,
+            options: {
+              A: "this is A",
+              B: "this is B",
+              C: "this is C",
+              D: "this is D",
             }
           }
-          console.log(this.answer);
+        ],
+        answer: [],
+      }
+    },
+    methods: {
+      selectOption(index, answer, isSingle) {
+        console.log(answer);
+        if (isSingle) {
+          //替换答案
+          this.$set(this.answer, 0, answer)
+        } else {
+          //判断是否已选，已选则删除，未选则添加
+          if (this.answer.indexOf(answer) != -1) {
+            console.log(this.answer.indexOf(answer));
+            var index = this.answer.indexOf(answer)
+            this.answer.splice(index, 1)
+          } else {
+            this.answer.push(answer)
+          }
         }
-      },
-    }
+        console.log(this.answer);
+      }
+    },
+    components: {},
+  }
 </script>
 
 <style scoped>
   @import "../../assets/test/options/A/iconfont.css";
   @import "../../assets/test/options/B/iconfont.css";
   @import "../../assets/test/options/C/iconfont.css";
-  
-  .active{
+
+  .active {
     background-color: #ccc;
   }
 </style>
